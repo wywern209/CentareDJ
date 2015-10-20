7a4b2dd9f25d74111623763343505e76166ebae4
Goutham Pai <goutham.pai@centare.com>
2015-10-20 15:17:58 -0500

    initial commit

---
+++ CentareDJ/CentareDJ.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CentareDJ.Web.MvcApplication" Language="C#" %>
---
+++ CentareDJ/CentareDJ.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CentareDJ.Web.Startup))]
+namespace CentareDJ.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
